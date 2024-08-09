1de6e2a6c2fccb46f2bf44fbb96692b54496545d
Krokodil_Gena <alex.usevich@gmail.com>
2024-08-09 21:46:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moviefrutty/App.kt
@@ -0,0 +1,6 @@
+package com.example.moviefrutty
+
+import android.app.Application
+
+class App: Application() {
+}
---
+++ app/src/main/java/com/example/moviefrutty/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.moviefrutty.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val LightGrey = Color(0xFFBDBCBC)
+val Grey = Color(0xFF777676)
+val Orange = Color(0xFFFF5722)
+val Blue = Color(0xFF2196F3)
